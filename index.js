0311dcec82d401997dce0f846e01651ea7c17d4d
Robin Blomberg <robin.blomberg@hiq.se>
2021-04-08 09:45:14 +0200

    Use CommonJS exports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /**
  * @see https://www.npmjs.com/package/express-async-handler
  */
-export const asyncHandler = (requestHandler) => {
+const asyncHandler = (requestHandler) => {
   const wrappedRequestHandler = (req, res, next) => {
     const result = requestHandler(req, res, next);
     return Promise.resolve(result).catch(next);
   };
   return wrappedRequestHandler;
 };
+
+module.exports = { asyncHandler };
